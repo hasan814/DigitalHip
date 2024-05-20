d30f2ebad67cb86b2933504cc1d2fd4a4ee2255d
hasan814 <h.mousavi910@gmail.com>
2024-05-20 15:09:43 +0330

    Added and Finished Register Section

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,7 @@
+import RegisterPage from "@/components/templates/Register/RegisterPage";
+
+const Register = () => {
+  return <RegisterPage />;
+};
+
+export default Register;
--- src/utils/action.js
+++ src/utils/action.js
@@ -4,6 +4,8 @@ import { signIn, signOut } from "./auth";
 import { revalidatePath } from "next/cache";
 import { connectDB } from "./connectDB";
 import { Post } from "@/models/Post";
+import { User } from "@/models/User";
+import { genSalt, hash } from "bcrypt";
 
 export const addPost = async (formData) => {
   const { title, desc, slug, userId } = Object.fromEntries(formData);
@@ -47,4 +49,33 @@ export const handlerGithubLogin = async () => {
 export const handleLogout = async () => {
   "use server";
   await signOut();
+};
+
+export const register = async (formData) => {
+  const { username, password, email, img, passwordRepeat } =
+    Object.fromEntries(formData);
+  console.log(formData);
+  if (password !== passwordRepeat) return "Passwords do not match";
+
+  try {
+    await connectDB();
+    const user = await User.findOne({ username });
+    if (user) return "Username already exist";
+
+    const salt = await genSalt();
+    const hashedPassword = await hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password,
+      img,
+    });
+
+    await newUser.save();
+    console.log("Saved to DB");
+  } catch (error) {
+    console.log(error);
+    return { error: "Something went wrong!" };
+  }
 };
