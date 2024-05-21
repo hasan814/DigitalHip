457228f82fa0fba42069e5ca5d0861828f725082
hasan814 <h.mousavi910@gmail.com>
2024-05-21 13:11:28 +0330

    Added Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,60 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { v4 as uuidv4 } from "uuid";
+import MaxWidthWrapper from "@/components/templates/MaxWidthWrapper";
+import Link from "next/link";
+import { perks } from "@/utils/helper";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Your marketplace for high-quality{" "}
+            <span className="text-blue-600">digital assets .</span>
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcome to DigitalHip. Every asset on our platform is verified by
+            our team to ensure our highest quality standards.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href={""} className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            <Button variant="ghost">Our quality promise &rarr;</Button>
+          </div>
+        </div>
+
+        {/* TODO: List products */}
+      </MaxWidthWrapper>
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+            {perks.map((perk) => (
+              <div
+                className="text-center md:flex md:items-start md:text-left lg:block lg:text-center "
+                key={uuidv4()}
+              >
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                    {perk.Icon}
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium text-gray-900">
+                    {perk.name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground`">
+                    {perk.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
+  );
 };
 
 export default Home;
